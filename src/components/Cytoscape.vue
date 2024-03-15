8a70a2fe9786abfbfaf4dba5e74081dc2792334b
david vincent eldridge <d.vincenteldridge@gmail.com>
2024-03-15 10:17:59 -0400

    instead of creating cytoscape onMounted made it a function that is currently called with watchEffect - so if props change reiniting is fired.

--- src/components/Cytoscape.vue
+++ src/components/Cytoscape.vue
@@ -1,28 +1,36 @@
 <script lang="ts" setup>
-  const props = withDefaults(defineProps<{
-    nodes?: NodeDefinition[];
-    edges?: EdgeDefinition[];
-  }>(), {
-    nodes: () => [],
-    edges: () => []
-  })
-  import { onMounted, ref } from "vue";
-  import cytoscape from "cytoscape";
-  import type { EdgeDefinition, NodeDefinition } from "cytoscape"
-  const cy = ref();
-  onMounted(() => {
-    cytoscape({
-      container: cy.value,
-      elements: {
-        nodes: props.nodes,
-        edges: props.edges
-      },
-      layout: {
-        name: 'preset'
-      },
-      style: fetch("./public/arrows.json").then((r) => r.json())
-    })
+import { watchEffect, ref } from "vue";
+import cytoscape from "cytoscape";
+import type { EdgeDefinition, NodeDefinition } from "cytoscape";
+
+const props = withDefaults(defineProps<{
+  nodes?: NodeDefinition[];
+  edges?: EdgeDefinition[];
+}>(), {
+  nodes: () => [],
+  edges: () => []
+})
+
+const cy = ref();
+const isLoading = ref(true);
+const initCytoscape = () => {
+  isLoading.value = true;
+  cytoscape({
+    container: cy.value,
+    elements: {
+      nodes: props.nodes,
+      edges: props.edges
+    },
+    layout: {
+      name: 'preset'
+    },
+    style: fetch("./public/arrows.json").then((r) => r.json())
   });
+  setTimeout(() => {
+    isLoading.value = false;
+  }, 2000 )
+}
+watchEffect(initCytoscape);
 </script>
 <template>
   <div ref="cy"
